f437b6205dc5deecf831d1b1792f93f4c2c5d92a
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-03-23 16:27:55 +0700

    Deploy docusaurus

--- KminBlog/src/components/PostList/PostList.tsx
+++ KminBlog/src/components/PostList/PostList.tsx
@@ -19,6 +19,28 @@ export default function PostList() {
             <PostItemReverse key={index} post={post} />
           ))}
         </div>
+        <div className="col-span-12 collapse">
+          <input type="checkbox" />
+          <div className="flex justify-center collapse-title">
+            <button className="flex justify-center items-center  text-black rounded-2xl px-3 py-2">
+              View more ...
+            </button>
+          </div>
+          <div className="collapse-content">
+            <div className="grid grid-cols-12 gap-2 -5">
+              <div className="col-span-6">
+                {posts.map((post, index) => (
+                  <PostItem key={index} post={post} />
+                ))}
+              </div>
+              <div className="col-span-6">
+                {posts.map((post, index) => (
+                  <PostItemReverse key={index} post={post} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="font-bold text-3xl mb-6">Blog</div>
       <div className="grid grid-cols-12 gap-2 -5">
---
+++ KminBlog/src/pages/hi.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function hi() {
+  return (
+    <div className="collapse bg-base-200">
+      <input type="checkbox" />
+      <div className="collapse-title text-xl font-medium">
+        Click me to show/hide content
+      </div>
+      <div className="collapse-content">
+        <p>hello</p>
+      </div>
+    </div>
+  );
+}
